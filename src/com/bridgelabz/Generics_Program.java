0c88f01e324ca576da6fada33bb34bf7774e2284
lohade <abhisheklohade8584@gmail.com>
2021-09-23 18:05:44 +0530

    find max float number

--- src/com/bridgelabz/Generics_Program.java
+++ src/com/bridgelabz/Generics_Program.java
@@ -5,6 +5,7 @@ public class Generics_Program{
 	// write your code here
         //System.out.println("");
         compare(15,11,2);
+        compare(1.2f,3.4f,2.0f);
     }
     public static void compare(Integer a,Integer b,Integer c){
         int max=a;
@@ -15,6 +16,16 @@ public class Generics_Program{
             max=c;
         }
         System.out.println("largest number is:"+max);
+    }
+    public static void compare(Float a,Float b,Float c){
+        float max=a;
+        if(b.compareTo(max)>0){
+            max=b;
+        }
+        if(c.compareTo(max)>0){
+            max=c;
+        }
+        System.out.println("largest float number is:"+max);
     }
 
 
