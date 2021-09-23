f231ffbba127dc66c02f8c4a067f543383457b82
lohade <abhisheklohade8584@gmail.com>
2021-09-23 18:16:16 +0530

    string max

--- src/com/bridgelabz/Generics_Program.java
+++ src/com/bridgelabz/Generics_Program.java
@@ -1,32 +1,44 @@
 package com.bridgelabz;
 
-public class Generics_Program{
+public class Generics_Program {
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         //System.out.println("");
-        compare(15,11,2);
-        compare(1.2f,3.4f,2.0f);
+        compare(15, 11, 2);
+        compareFloat(1.2f, 3.4f, 2.0f);
+        compareString("Apple","Banana","Peach");
     }
-    public static void compare(Integer a,Integer b,Integer c){
-        int max=a;
+
+    public static void compare(Integer a, Integer b, Integer c) {
+        int max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        System.out.println("largest number is:" + max);
+    }
+
+    public static void compareFloat(Float a, Float b, Float c) {
+        float max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        System.out.println("largest float value:" + max);
+    }
+    public static void compareString(String a,String b,String c){
+        String max=a;
         if(b.compareTo(max)>0){
             max=b;
         }
         if(c.compareTo(max)>0){
             max=c;
         }
-        System.out.println("largest number is:"+max);
+        System.out.println("largest string is:"+max);
     }
-    public static void compare(Float a,Float b,Float c){
-        float max=a;
-        if(b.compareTo(max)>0){
-            max=b;
-        }
-        if(c.compareTo(max)>0){
-            max=c;
-        }
-        System.out.println("largest float number is:"+max);
-    }
-
-
 }
+
