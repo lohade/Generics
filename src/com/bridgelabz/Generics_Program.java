a7dd1647730026e8212992223a25ddab8f15ff6b
lohade <abhisheklohade8584@gmail.com>
2021-09-23 19:42:39 +0530

    generics array

--- src/com/bridgelabz/Generics_Program.java
+++ src/com/bridgelabz/Generics_Program.java
@@ -1,22 +1,31 @@
 package com.bridgelabz;
 
-public class Generics_Program {
-    public static void main(String[] args) {
-        System.out.println("largest integer value:"+compare(15,11,2));
-        System.out.println("largest float value:" + compare(1.2f,3.4f,2.0f));
-        System.out.println("largest strings:"+compare("Apple","Banana","Peach"));
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Generics_Program <E extends Comparable<E>> {
+    E[] elements;
+
+
+    public Generics_Program(E[] elements){
+        this.elements=elements;
+
     }
 
 
-    public static <E extends Comparable<E>> E compare(E a, E b, E c){
-        E max = a;
-        if(b.compareTo(max)>0){
-            max = b;
-        }
-        if(c.compareTo(max)>0){
-            max = c;
-        }
+    public static void main(String[] args) {
+        Integer[] array1={15,11,2};
+        Float[] array2={1.2f,3.4f,2.0f};
+        System.out.println(compare(array1));
+        System.out.println( compare(array2));
+    }
+
+    public static <E extends Comparable<E>> E compare(E[] elements){
+        Arrays.sort(elements);
+        int len= elements.length;
+        E max=elements[len-1];
         return max;
+
 
     }
 }
