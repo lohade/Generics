3ecadc0d3701b3733c0e17e4f7f619e212fcd594
lohade <abhisheklohade8584@gmail.com>
2021-09-23 18:43:25 +0530

    use generics method

--- src/com/bridgelabz/Generics_Program.java
+++ src/com/bridgelabz/Generics_Program.java
@@ -2,43 +2,22 @@ package com.bridgelabz;
 
 public class Generics_Program {
     public static void main(String[] args) {
-        // write your code here
-        //System.out.println("");
-        compare(15, 11, 2);
-        compareFloat(1.2f, 3.4f, 2.0f);
-        compareString("Apple","Banana","Peach");
+        System.out.println("largest integer value:"+compare(15,11,2));
+        System.out.println("largest float value:" + compare(1.2f,3.4f,2.0f));
+        System.out.println("largest strings:"+compare("Apple","Banana","Peach"));
     }
 
-    public static void compare(Integer a, Integer b, Integer c) {
-        int max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        System.out.println("largest number is:" + max);
-    }
 
-    public static void compareFloat(Float a, Float b, Float c) {
-        float max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        System.out.println("largest float value:" + max);
-    }
-    public static void compareString(String a,String b,String c){
-        String max=a;
+    public static <E extends Comparable<E>> E compare(E a, E b, E c){
+        E max = a;
         if(b.compareTo(max)>0){
-            max=b;
+            max = b;
         }
         if(c.compareTo(max)>0){
-            max=c;
+            max = c;
         }
-        System.out.println("largest string is:"+max);
+        return max;
+
     }
 }
 
